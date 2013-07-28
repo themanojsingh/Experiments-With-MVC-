2155f52a45c143d3881b9cbf5d14f4978fa1fb06
Siddharth Kosta <siddharth.kosta@gmail.com>
2013-07-28 22:54:30 +0530

    Persist Menu selection and dynamic Jquery grid with menu to push values

---
+++ MvcApplication1/MvcApplication1/App_Start/NavigationHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Html;
+using System.Web.Mvc;
+
+namespace MvcApplication1.App_Start
+{
+    public static class NavigationHelper
+    {
+        public static MvcHtmlString MenuLink(this System.Web.Mvc.HtmlHelper helper,
+    string text, string action, string controller)
+        {
+            var routeData = helper.ViewContext.RouteData.Values;
+            var currentController = routeData["controller"];
+            var currentAction = routeData["action"];
+
+            if (String.Equals(action, currentAction as string,
+                      StringComparison.OrdinalIgnoreCase)
+                &&
+               String.Equals(controller, currentController as string,
+                       StringComparison.OrdinalIgnoreCase))
+            {
+                return helper.ActionLink(
+                    text, action, controller, null,
+                    new { @class = "currentMenuItem" }
+                    );
+            }
+            return helper.ActionLink(text, action, controller);
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            List<Cell> lstCell = new List<Cell>()
+            {
+                new Cell{ X = 6, Y= 6}
+            };
+
+            return View(lstCell);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
